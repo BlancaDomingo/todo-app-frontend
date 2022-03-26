2d5de0cd62d2dc523064556f77fba2adbced8f3f
BLANCA DOMINGO <blaaanca@yahoo.es>
2022-03-26 15:33:04 +0100

    Logout

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,44 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import RegisterPage from "./pages/RegisterPage";
 import LoggedIn from "./components/LoggedIn";
-import './scss/App.scss';
+import "./scss/App.scss";
 
 function App() {
   const [token, setToken] = useState("");
   const [userName, setUserName] = useState("");
-  const [userId, setUserId] = useState("")
+  const [userId, setUserId] = useState("");
 
   const onLogin = (data) => {
     const freshToken = data.token;
     if (freshToken) {
       setToken(freshToken);
       setUserName(data.user.firstName);
-      setUserId(data.user._id)
+      setUserId(data.user._id);
     }
   };
+
+  const onLogout = () => {
+    setToken("");
+    setUserName("");
+    setUserId("");
+  };
+
   return (
     <>
-       <LoggedIn userName={userName} />
+      <LoggedIn userName={userName} onLogout={onLogout} />
       <div className="superdiv">
-       
         <div className="App">
           <Header />
           <Routes>
             <Route path="/" element={<LoginPage onLogin={onLogin} />} />
-            <Route path="/register" element={<RegisterPage onLogin={onLogin} />} />
-            <Route path="/todoApp" element={<TodosPage token={token} userId={userId} />} />
+            <Route
+              path="/register"
+              element={<RegisterPage onLogin={onLogin} />}
+            />
+            <Route
+              path="/todoApp"
+              element={<TodosPage token={token} userId={userId} />}
+            />
             <Route path="/todoApp/Hilfe" element={<Help />} />
             <Route path="*" element={<LoginPage onLogin={onLogin} />} />
           </Routes>
--- src/components/LoggedIn.jsx
+++ src/components/LoggedIn.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import "../scss/LoggedIn.scss";
+import { useNavigate } from "react-router-dom";
 
-export default function LoggedIn({ userName }) {
+
+export default function LoggedIn({ userName, onLogout }) {
+
+  const navigate = useNavigate();
+
+  function logOut() {   
+    onLogout(); 
+    navigate("/");// nicht richtig
+  }
+
   return (
     <div className="loggedIn-div">
       {userName.length > 0 && <p>
         Hallo <span className="userName">{userName} </span>{" "}
-        <button className="ButtonLogout">Ausloggen</button>
+        <button className="ButtonLogout" onClick={logOut}>
+            Ausloggen
+          </button>
+       
       </p>}
     </div>
   );
 }
+
